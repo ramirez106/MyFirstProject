12d51fee6d7ded4185c83063c2a07be9ef88141d
ramirez106 <112105048+ramirez106@users.noreply.github.com>
2022-08-26 08:25:27 -0700

    Add project files.

---
+++ MyFirstProject/Source.cpp
@@ -0,0 +1,16 @@
+/*
+	Carlos Ramirez
+	C++ Fall 2022
+	Due: Aug 31, 2022
+	Lab 1 Introduction to C++ Programming
+	This lab is a demo of my ability to work with an IDE an GitHub
+	*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World" << " Blue";
+
+	return0;
+}
